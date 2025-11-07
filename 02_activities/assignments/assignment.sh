09d9363b1c7a9f4b6730100f5c7e267ef4605193
stella-hoang <239895800+stella-hoang@users.noreply.github.com>
2025-11-06 21:59:16 -0500

    18

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -52,7 +52,8 @@ rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt
-ls -r ./data/processed > ./data/inventory.txt
+ls -R ./data/processed > ./data/inventory.txt
+cat ./data/inventory.txt
 
 ###########################################
 
